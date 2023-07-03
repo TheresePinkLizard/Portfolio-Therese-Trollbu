878ae62e6ddec00cfa4bbf83fec5a3c01be2dab0
TheresePinkLizard <therese.trollbu@gmail.com>
2023-07-03 12:32:20 +0200

    fds

--- src/main/java/com/example/homepage/PortfolioApplication.java
+++ src/main/java/com/example/homepage/PortfolioApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HomepageApplication {
+public class PortfolioApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(HomepageApplication.class, args);
+        SpringApplication.run(PortfolioApplication.class, args);
     }
 
 }
